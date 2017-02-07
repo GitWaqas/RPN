508d67a0bbe603584bc1ed6d5708ff7004a11494
Waqas <cph-wm66@cphbusiness.dk>
2017-02-07 10:25:08 +0100

    first commit

--- RPN/src/main/java/RPN.java
+++ RPN/src/main/java/RPN.java
@@ -4,7 +4,10 @@
     import java.util.Stack ;
 
 public class RPN {
-
+/*
+    A very basic calculator which uses stack to push and pop numbers from bufferedReader, 
+    if operand is found then last two numbers are popped and result is pushed back to stack.
+    */
 
   
     public static void main(String[] args) throws IOException {
@@ -13,7 +16,7 @@ public class RPN {
         while (true) {
             String line = reader.readLine();
             String[] symbols = line.split(" "); // adds multiple values i.e 20 30 +
-            for (String symbol : symbols) {  // runs a loop untill symbols array is empty
+            for (String symbol : symbols) {  // runs a loop for all symbols in array
                 switch (symbol) {
                     case "+":
                         System.out.println("add operation");
@@ -36,7 +39,7 @@ public class RPN {
                         System.exit(1);
                     default:
                         System.out.println("Added to stack: " +symbol);
-                        stack.push(Double.parseDouble(symbol)); // if not one of the other cases, it is assumed that it's a number.
+                        stack.push(Double.parseDouble(symbol)); // if not one of the other cases, it is assumed that it's a number
                         break;
                 }
                 System.out.println("stack content: " +stack);
